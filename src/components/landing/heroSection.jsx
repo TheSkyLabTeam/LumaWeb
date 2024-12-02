2a791a4bc704afed2daae160f34a3dd08f76cb32
daniherreraa <herreraacevedodl@gmail.com>
2024-12-02 18:40:27 -0500

    feat: Agregar nuevo ícono y actualizar la sección de héroe en la página de inicio

--- src/components/landing/heroSection.jsx
+++ src/components/landing/heroSection.jsx
@@ -1,35 +1,57 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/navigation";
-import '@/components/landing/landingStyles.css'
+import "@/components/landing/landingStyles.css";
+import { Button } from "../ui/button";
 
 const HeroSection = () => {
   const t = useTranslations("Landing");
   return (
-    <header className="relative w-full h-[100svh] flex n">
-      {/* Titles */}
-      <div
-        id="landingTitlesContainer"
-        className="z-10 w-full flex flex-col text-center items-center absolute p-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      >
-        <h1 className="w-full md:w-[70svw] text-3xl md:text-5xl font-clash font-bold text-on-background-dark/95">
-          {t("title")}
+    <header className="relative w-full h-[100svh] flex overflow-hidden">
+      <div className="block z-10 w-full h-full mix-blend-exclusion antialiased text-[#A0FFFF]">
+        <nav className="flex p-6 flex-row justify-between items-center">
+          <div id="lumaBrandContainer">
+            <Link href={'/dashboard'}>
+              <img className="w-8 h-auto" src="/images/LumaIcon.svg" alt="Luma Icon" />
+            </Link>
+          </div>
+          <div id="linksContainer" className="flex flex-row font-archivo font-normal  gap-6">
+            <Link href={'/'}>HOW IT WORKS</Link>
+            <Link href={'/'}>TEAM</Link>
+            <Link href={'/'}>CONTACT</Link>
+          </div>
+          <div id="getStartedContainer">
+            <Button className="border rounded-xl">
+              <p className="font-archivo font-semibold">Get started</p>
+            </Button>
+          </div>
+        </nav>
+        <h1 className="absolute text-[20vw] font-clash font-semibold
+            top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          LUMA
         </h1>
-        <h3 className="mt-6 md:w-[70svw] lg:w-[50svw] font-archivo text-base md:text-xl text-on-background-dark/95">
-          {t("subtitle")}
-        </h3>
-        <div id="headerCtASContainer" className="w-fit flex gap-2 mt-8">
-          <Link href={"/dashboard"}>
-            <button className=" font-archivo text-on-surface-dark border-2 border-outline-variant-dark bg-surface-container-lowest-dark hover:bg-surface-container-low-dark transition-colors">
-              {t("aboutcta")}
-            </button>
-          </Link>
-          <Link href={"/dashboard"}>
-            <button className=" font-archivo text-on-surface bg-white hover:bg-surface-dim transition-colors">
-              {t("headercta")}
-            </button>
-          </Link>
+        <div className="absolute bottom-6 flex flex-row w-full justify-between items-end px-6">
+          <div>
+            <p className="font-archivo font-light text-[0.75vw] max-w-[20vw] leading-3">
+              UNA NUEVA FORMA DE ESTUDIAR LA ACTIVIDAD SOLAR. <br /> IMAGENES DE
+              ALTA CALIDAD <br /> +10 AÑOS EN DATOS <br /> DAHSBOARD INTUITIVA
+            </p>
+          </div>
+          <div>
+            <img
+              src="/images/labelFacilities.svg"
+              alt="Created at the AstroUTB facilities"
+            />
+          </div>
         </div>
       </div>
+      <video
+        autoPlay
+        muted
+        loop
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+      >
+        <source src="/videos/bgSunVideo.mp4" type="video/mp4" />
+      </video>
     </header>
   );
 };
