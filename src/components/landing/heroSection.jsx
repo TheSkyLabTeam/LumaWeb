a67e618f48ab48b2636430055df31cdb437388c0
daniherreraa <herreraacevedodl@gmail.com>
2024-12-03 16:11:33 -0500

    feat: Mejorar la sección de héroe con nuevas animaciones y agregar componente de navegación

--- src/components/landing/heroSection.jsx
+++ src/components/landing/heroSection.jsx
@@ -1,31 +1,37 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import { Link } from "@/navigation";
 import "@/components/landing/landingStyles.css";
-import { Button } from "../ui/button";
 import { useEffect } from "react";
 import gsap from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
+import { LandingNav } from "./landingnav";
 gsap.registerPlugin(TextPlugin);
 
 const HeroSection = () => {
   const t = useTranslations("Landing");
 
   useEffect(() => {
-    const timeline = gsap.timeline();
-
     // Labels animations
     gsap.to("#lumaFeaturesLabel", {
       text:
         "UNA NUEVA FORMA DE ESTUDIAR LA ACTIVIDAD SOLAR. \n IMAGENES DE ALTA CALIDAD \n +10 AÑOS EN DATOS \n DAHSBOARD INTUITIVA.",
-      duration: 2
+      duration: 1
     });
 
     gsap.to("#astroFacilities", {
       text:
         "PRODUCT CREATED IN THE FACILITIES \n OF THE UTB’S ASTRONOMY AND DATA \n SCIENCE RESEARCH SEEDBED.",
-      duration: 2
+      duration: 1
+    });
+
+    // Circles animations
+    [...Array(6)].forEach((_, index) => {
+      gsap.to(`#circle-${index}`, {
+        x: -100 + index * 20, // Posición final
+        duration: 0.6,
+        ease: "back.inOut(1.7)"
+      });
     });
 
     ["L", "U", "M", "A"].forEach((letter, index) => {
@@ -40,30 +46,7 @@ const HeroSection = () => {
   return (
     <header className="relative w-full h-[100svh] flex overflow-hidden">
       <div className="block z-10 w-full h-full mix-blend-exclusion antialiased text-[#A0FFFF]">
-        <nav className="flex p-6 flex-row justify-between items-center">
-          <div id="lumaBrandContainer">
-            <Link href={"/"}>
-              <img
-                className="w-8 h-auto"
-                src="/images/LumaIcon.svg"
-                alt="Luma Icon"
-              />
-            </Link>
-          </div>
-          <div
-            id="linksContainer"
-            className="flex flex-row font-archivo font-normal gap-6"
-          >
-            <Link href={"/"}>HOW IT WORKS</Link>
-            <Link href={"/"}>TEAM</Link>
-            <Link href={"/"}>CONTACT</Link>
-          </div>
-          <div id="getStartedContainer">
-            <Button className="border rounded-none">
-              <p className="font-archivo font-semibold">Get started</p>
-            </Button>
-          </div>
-        </nav>
+        <LandingNav />
         <div className="absolute flex flex-row items-center text-[20vw] font-clash font-semibold
              top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden h-[1em]">
           {"LUMA".split("").map((letter, index) =>
@@ -86,11 +69,10 @@ const HeroSection = () => {
           </div>
           <div>
             <div className="flex w-full justify-end items-baseline">
-              <p className="font-archivo font-light text-xs italic">2024.</p>
-              <div className="relative w-16 h-16">
+              <div className="relative w-16 h-16 mb-2">
                 {[...Array(6)].map((_, index) =>
                   <div
-                    key={index}
+                    id={`circle-${index}`}
                     className="absolute top-0 left-0 w-16 h-16 bg-none border-white border-2 rounded-full"
                   />
                 )}
--- src/components/landing/landingnav.jsx
+++ src/components/landing/landingnav.jsx
@@ -1,47 +1,79 @@
+import { useRef } from "react";
 import { Link } from "@/navigation";
-import Image from "next/image";
+import { Button } from "../ui/button";
+import gsap from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { useGSAP } from "@gsap/react";
+gsap.registerPlugin([TextPlugin, useGSAP]);
 
 export const LandingNav = () => {
+  const container = useRef();
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    // Links animations
+    [...Array(3)].forEach((_, index) => {
+      gsap.to(`#navLink-${index}`, {
+        y: 0,
+        duration: 0.6 + index * 0.3,
+        ease: "back.inOut(1.7)",
+      });
+    });
+
+    tl.to("#lumaBrandContainer", {
+      x: 0,
+      duration: 0.6,
+      ease: "back.inOut(1.7)",
+    })
+      .to(
+        "#getStartedContainer",
+        {
+          x: 0,
+          duration: 0.6,
+          ease: "back.inOut(1.7)",
+        },
+        "-=0.4" // Animación simultánea con un pequeño solapamiento
+      );
+  });
+
   return (
-    <nav className="
-      font-archivo
-      fixed top-6 left-1/2 transform -translate-x-1/2 
-      flex items-center w-fit h-fit px-4 py-3 space-x-6 
-      bg-surface-container-lowest-dark border-2 border-outline-variant-dark
-      rounded-lg z-50"
+    <nav
+      ref={container}
+      className="flex p-6 flex-row justify-between items-center"
     >
-      {/* Luma logo */}
-      <div id="brandContainer" className="flex items-center h-full">
-        <svg
-          className="w-4 h-auto"
-          width="190"
-          height="236"
-          viewBox="0 0 190 236"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M40.6484 127.177C40.6484 130.096 37.6266 132.013 35.0964 130.557C14.1222 118.488 -1.65548e-05 95.851 -1.65548e-05 69.9152C-1.65548e-05 31.3021 31.3021 0 69.9152 0C104.984 0 134.022 25.819 139.058 59.485C139.405 61.8042 137.553 63.818 135.208 63.818L44.6484 63.818C42.4392 63.818 40.6484 65.6088 40.6484 67.818V127.177Z"
-            fill="#dfe3e7"
+      <div id="lumaBrandContainer" className="-translate-x-32">
+        <Link href={"/"}>
+          <img
+            className="w-8 h-auto"
+            src="/images/LumaIcon.svg"
+            alt="Luma Icon"
           />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M148.451 107.879C148.451 104.959 151.473 103.042 154.003 104.498C174.977 116.567 189.1 139.205 189.1 165.14C189.1 203.753 157.798 235.056 119.184 235.056C84.1158 235.056 55.0776 209.236 50.0419 175.571C49.695 173.251 51.5468 171.238 53.8918 171.238L144.451 171.238C146.66 171.238 148.451 169.447 148.451 167.238L148.451 107.879Z"
-            fill="#dfe3e7"
-          />
-          <rect x="50" y="73" width="89" height="89" rx="6" fill="#dfe3e7" />
-        </svg>
-      </div>
-      {[["Home", "/"], ["Dashboard", "/dashboard"], ["About", "/about"]].map(([text, url]) =>
-        <Link href={url} key={url}>
-          <p className="text-sm text-on-surface-variant-dark/60 hover:text-on-surface-variant-dark/100 transition-opacity">
-            {text}
-          </p>
         </Link>
-      )}
+      </div>
+      <div
+        id="linksContainer"
+        className="flex flex-row font-archivo font-normal gap-8"
+      >
+        {[
+          ["HOW IT WORKS", "/"],
+          ["TEAM", "/"],
+          ["CONTACT", "/"],
+        ].map(([text, href], index) => (
+          <Link
+            className="-translate-y-32"
+            id={`navLink-${index}`}
+            key={index}
+            href={href}
+          >
+            {text}
+          </Link>
+        ))}
+      </div>
+      <div id="getStartedContainer" className="translate-x-36">
+        <Button className="border rounded-none">
+          <p className="font-archivo font-semibold">Get started</p>
+        </Button>
+      </div>
     </nav>
   );
 };
