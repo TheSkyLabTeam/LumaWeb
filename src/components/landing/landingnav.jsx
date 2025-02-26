cdfc458fa245abe5a05c8a77ba534efc9205bdae
daniherreraa <herreraacevedodl@gmail.com>
2025-02-26 10:46:46 -0500

    add language switcher and update navigation; implement language toggle and enhance navbar with new links

--- src/components/landing/landingnav.jsx
+++ src/components/landing/landingnav.jsx
@@ -1,16 +1,24 @@
 import { useRef } from "react";
-import { Link } from "@/navigation";
+import { Link, useRouter, usePathname } from "@/navigation";
 import { Button } from "../ui/button";
 import gsap from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
-import {useTranslations} from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 gsap.registerPlugin([TextPlugin, useGSAP]);
 
 export const LandingNav = () => {
   const t = useTranslations("Landing");
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentLocale = useLocale();
   const container = useRef();
+
+  const switchLanguage = (locale) => {
+    router.replace(pathname, { locale });
+  };
+
   useGSAP(() => {
     const tl = gsap.timeline();
 
@@ -24,64 +32,79 @@ export const LandingNav = () => {
     });
 
     tl
-      .to("#lumaBrandContainer", {
-        x: 0,
-        duration: 0.6,
-        ease: "back.inOut(1.7)"
-      })
-      .to(
-        "#getStartedContainer",
-        {
+        .to("#lumaBrandContainer", {
           x: 0,
           duration: 0.6,
           ease: "back.inOut(1.7)"
-        },
-        "-=0.4" // Animación simultánea con un pequeño solapamiento
-      );
+        })
+        .to(
+            "#getStartedContainer",
+            {
+              x: 0,
+              duration: 0.6,
+              ease: "back.inOut(1.7)"
+            },
+            "-=0.4" // Animación simultánea con un pequeño solapamiento
+        );
   });
 
   return (
-    <nav
-      ref={container}
-      className="flex p-4 md:p-6 flex-row justify-between items-center"
-    >
-      <div id="lumaBrandContainer" className="-translate-x-32">
-        <Link href={"/"}>
-          <Image
-            width={32}
-            height={32}
-            className="w-8 h-auto"
-            src="/images/LumaIcon.svg"
-            alt="Luma Icon"
-          />
-        </Link>
-      </div>
-      <div
-        id="linksContainer"
-        className="hidden md:flex flex-row font-archivo font-normal gap-8"
+      <nav
+          ref={container}
+          className="flex p-4 md:p-6 flex-row justify-between items-center"
       >
-        {[
-          [t('headerWorkLink'), "/"],
-          [t('headerTeamLink'), "/"],
-          [t('headerCredits'), "/credits"],
-        ].map(([text, href], index) =>
-          <Link
-            className="-translate-y-32"
-            id={`navLink-${index}`}
-            key={index}
-            href={href}
-          >
-            {text}
+        <div id="lumaBrandContainer" className="flex flex-row items-center -translate-x-32">
+          <Link href={"/"}>
+            <Image
+                width={32}
+                height={32}
+                className="w-8 h-auto"
+                src="/images/LumaIcon.svg"
+                alt="Luma Icon"
+            />
           </Link>
-        )}
-      </div>
-      <div id="getStartedContainer" className="translate-x-36">
-        <Link href={"/dashboard"}>
-          <Button className="border rounded-none bg-[#A0FFFF] hover:bg-white">
-            <p className="font-archivo font-semibold text-black">Get started</p>
-          </Button>
-        </Link>
-      </div>
-    </nav>
+        </div>
+        <div
+            id="linksContainer"
+            className="hidden md:flex flex-row font-archivo font-normal gap-8"
+        >
+          {[
+            [t('headerWorkLink'), "/"],
+            [t('headerTeamLink'), "/"],
+            [t('headerCredits'), "/credits"],
+          ].map(([text, href], index) =>
+              <Link
+                  className="-translate-y-32"
+                  id={`navLink-${index}`}
+                  key={index}
+                  href={href}
+              >
+                {text}
+              </Link>
+          )}
+        </div>
+        <div id="getStartedContainer" className="flex flex-row items-center gap-6 translate-x-52">
+          <div className="font-archivo flex gap-1 cursor-pointer">
+          <span
+              onClick={() => switchLanguage("es")}
+              className={currentLocale === "es" ? "font-extrabold" : "hover:text-gray-400"}
+          >
+            ES
+          </span>
+            <span>/</span>
+            <span
+                onClick={() => switchLanguage("en")}
+                className={currentLocale === "en" ? "font-extrabold" : "hover:text-gray-400"}
+            >
+            EN
+          </span>
+          </div>
+          <Link href={"/dashboard"}>
+            <Button className="border rounded-none bg-[#A0FFFF] hover:bg-white">
+              <p className="font-archivo font-semibold text-black">{t('headerCta')}</p>
+            </Button>
+          </Link>
+        </div>
+      </nav>
   );
 };
