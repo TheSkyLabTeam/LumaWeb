8f9054f318217ff539160702c7fe2ea755b4935c
daniherreraa <herreraacevedodl@gmail.com>
2025-02-26 10:10:57 -0500

    add credits page; localize content and implement translations for English and Spanish

--- src/app/[locale]/credits/page.jsx
+++ src/app/[locale]/credits/page.jsx
@@ -1,4 +1,7 @@
+import {useTranslations} from "next-intl";
+
 export default function CreditsPage() {
+    const t = useTranslations("CreditsPage");
     return (
         <div className="w-svw h-svh bg-background dark:bg-background-dark relative flex justify-center items-center">
             <video
@@ -7,39 +10,26 @@ export default function CreditsPage() {
                 loop
                 className="absolute top-0 left-0 w-full h-full object-cover z-0 saturate-0 brightness-50"
             >
-                <source src="/videos/bgSunVideo.mp4" type="video/mp4" />
+                <source src="/videos/bgSunVideo.mp4" type="video/mp4"/>
             </video>
             <div className="max-w-3xl mx-auto px-6 relative">
-                <h1 className="text-4xl mb-8 text-cyan-300 font-clash font-semibold">Créditos</h1>
+                <h1 className="text-4xl mb-8 text-cyan-300 font-clash font-semibold">{t('title')}</h1>
 
                 <div className="space-y-6 text-justify font-normal font-archivo text-white">
                     <p>
-                        LUMA utiliza imágenes del Observatorio Solar y Heliosférico SOHO (Solar and Heliospheric
-                        Observatory) para
-                        realizar sus análisis y obtener parámetros de actividad solar. SOHO es un proyecto de
-                        cooperación
-                        internacional entre la ESA (Agencia Espacial Europea) y la NASA.
+                        {t('paragraph1')}
                     </p>
 
                     <p>
-                        Las imágenes utilizadas en nuestra aplicación provienen de diferentes instrumentos a bordo del
-                        SOHO,
-                        incluyendo el Extreme ultraviolet Imaging Telescope (EIT) que captura imágenes del Sol en
-                        diferentes
-                        longitudes de onda del ultravioleta extremo, y el Michelson Doppler Imager (MDI) que proporciona
-                        imágenes de
-                        la fotosfera solar.
+                        {t('paragraph2')}
                     </p>
 
                     <p>
-                        Agradecemos al equipo de SOHO por hacer estas imágenes disponibles para la comunidad científica
-                        y educativa.
-                        Las imágenes de SOHO son cruciales para nuestro trabajo de análisis y comprensión de la
-                        actividad solar.
+                        {t('paragraph3')}
                     </p>
 
                     <div className="mt-16 text-center text-sm text-cyan-300 font-archivo">
-                        Hecho con ❤️ desde el semillero de astronomía y ciencia de datos de la UTB
+                        {t('footer')}
                     </div>
                 </div>
             </div>
